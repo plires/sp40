4960de856173e11da257d01c16efcea07bda3488
Pablo Lires <pablolires@gmail.com>
2022-04-14 11:42:07 -0300

    Estructuracion en html de todo el sitio

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,69 @@
 <footer class="container-fluid">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        FOOTER 
-        FOOTER 
-        FOOTER 
-        FOOTER 
-        FOOTER 
-        FOOTER 
+  <div class="row">
+    <div class="col-md-10 offset-md-1">
+      <div class="row">
+        
+        <div class="col-md-3">
+          <img class="img-fluid" src="./img/auto-footer.jpg" alt="car footer sp40">
+        </div>
+
+        <div class="col-md-3">
+          <p>
+            All rights reserved &copy; 2022<br>
+            SP40 by Iconic Auto Sports.<br>
+            <a 
+              class="transition" 
+              rel="noopener" 
+              target="_blank" 
+              href="mailto:info@sp40restomod.com">info@sp40restomod.com
+            </a><br>
+            <a 
+              class="transition" 
+              rel="noopener" 
+              target="_blank" 
+              href="tel:393463799155">+39 346 3799155
+            </a>
+          </p>
+        </div>
+
+        <div class="col-md-4">
+          <div class="row">
+            <div class="col-md-6">
+              <ul>
+                <li><a href="#">Presentation</a></li>
+                <li><a href="#">Concept</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Gallery</a></li>
+              </ul>
+            </div>
+
+            <div class="col-md-6">
+              <img class="img-fluid" src="./img/logo-footer.png" alt="logo footer sp40">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-2">
+          <ul>
+            <li>
+              <a href="#" rel="noopener" target="_blank">
+                <i class="fa-brands fa-facebook-square"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" rel="noopener" target="_blank">
+                <i class="fa-brands fa-instagram-square"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" rel="noopener" target="_blank">
+                <i class="fa-brands fa-linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   </div>
--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,34 @@
-<header>
+<header style="z-index: 9999;">
 
-  <div class="content_logo">
-    <a href="./">
-      <img class="logo" src="./img/logo-small-sp40.png" alt="logo small sp40">
-    </a>
+  <i id="hamburger" class="fas fa-bars transition"></i>
 
-    <i id="hamburger" class="fas fa-bars transition"></i>
-  </div>
+  <a href="./">
+    <img class="logo" src="./img/logo-small-sp40.png" alt="logo small sp40">
+  </a>
+
+  <a class="transition" href="<?= RRSS_INSTAGRAM ?>" target="_blank" rel="noopener noreferrer">
+    <i class="fab fa-instagram"></i>
+  </a>
 
   <nav id="menu" class="transition">
     <ul id="navigation">
-      <!-- <li><a class="transition" href="./">ABOUT US</a></li> -->
-      <li><a class="transition" href="#" data-bs-toggle="modal" data-bs-target="#contactModal">CONTACT</a></li>
       <li>
-        <a class="transition" href="<?= RRSS_INSTAGRAM ?>" target="_blank" rel="noopener noreferrer">
-          <i class="fab fa-instagram"></i>
-        </a>
+        <img class="logo" src="./img/logo-header.png" alt="logo header sp40">
+      </li>
+      <li>Presentation</li>
+      <li>Concept</li>
+      <li>Features</li>
+      <li>About us</li>
+      <li>Gallery</li>
+      <li>
+        <div class="icons">
+          <a class="transition" href="<?= RRSS_INSTAGRAM ?>" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+          <a class="transition" href="mailto:<?= EMAIL_CLIENT ?>" target="_blank" rel="noopener noreferrer">
+            <i class="fa-regular fa-envelope"></i>
+          </a>
+        </div>
       </li>
     </ul>
   </nav>
